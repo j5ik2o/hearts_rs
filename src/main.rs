f4b8cee3b9b64836388264e232d85c20433abf88
Hajime Fujita <hajime.fujita@gmail.com>
2022-09-30 22:25:22 +0900

    Removed bugs.

--- src/main.rs
+++ src/main.rs
@@ -16,44 +16,36 @@ const NUM_PLAYERS: usize = 4;
 
 fn main() {
 
-    // 4人のプレイヤー（RandomAgent）をインスタンス化し、オブジェクトをVecに格納する
+    // Making instances of 4 players and store the objects in Vec
     let mut players = Vec::new();
     for _ in 0..NUM_PLAYERS {
         let hands: [i32; NUM_KC] = [-1; NUM_KC];
         players.push(RandomAgent{hands: hands});
     }
 
-    // NUM_GAMES だけゲームをプレイする
+    // Playing NUM_GAMES times game
     for _ in 1..=NUM_GAMES {
-        play_one_game(players);
+
+        // Deal cards to each player
+        deal_cards(&mut players);
+        
+        for i in 0..NUM_PLAYERS {
+            println!("{:?}", players[i].hands);
+        }
+        
     }
     
 }
 
 
-fn play_one_game(players: Vec<RandomAgent>) {
-
-    // 各プレイヤーにカードを配る
-    distribute_cards(players);
-    
-}
-
-
-fn distribute_cards(players: Vec<RandomAgent>) {
+fn deal_cards(players: &mut Vec<RandomAgent>) {
 
     let mut v: Vec<i32> = (0..NUM_CARDS as i32).collect();
     let mut rng = rand::thread_rng();
     v.shuffle(&mut rng);
-
+    
     for i in 0..NUM_PLAYERS {
-        // println!("{:?}", &v[0..13]);
-        players[i as usize].set_hands(&v[0..13]);
-        
-        // println!("{:?}", v.split_at(NUM_KC).0);
-        // let cards = vec!(v.split_at(NUM_KC as usize).0);
-        // println!("{:?}", cards)
-        // players[i as usize].set_hands(v.split_at(NUM_KC).0);
-        // players[i as usize].set_hands(cards);
+        players[i].set_hands(&v[(i * NUM_KC)..((i+1) * NUM_KC)]);
     }
     
 }
@@ -67,9 +59,7 @@ struct RandomAgent {
 impl RandomAgent {
 
     fn set_hands(&mut self, cards: &[i32]) {
-        for i in 0..NUM_KC {
-            self.hands[i] = cards[i];
-        }
+        self.hands = cards.try_into().unwrap();
     }
     
 }
