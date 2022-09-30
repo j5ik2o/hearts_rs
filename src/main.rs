9ce1da874632c04e1aa707924e7361f76f89699d
Hajime Fujita <hajime.fujita@gmail.com>
2022-09-30 23:17:37 +0900

    Completed implementation of RandomAgent. Implemented the selection of the leading player in the first trick.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-// use rand::Rng;
+use rand::Rng;
 use rand::seq::SliceRandom;
 
 // Total number of games
@@ -10,24 +10,51 @@ const NUM_KC: usize = 13;
 // Number of all cards
 const NUM_CARDS: usize = NUM_KC * 4;
 
-// Number of players: HEARTS expects to be played by four players.
+// Number of players; HEARTS expects to be played by four players.
 const NUM_PLAYERS: usize = 4;
 
 
 fn main() {
 
-    // Making instances of 4 players and store the objects in Vec
+    // Making instances of 4 agents and store the objects in Vec.
     let mut players = Vec::new();
     for _ in 0..NUM_PLAYERS {
         let hands: [i32; NUM_KC] = [-1; NUM_KC];
         players.push(RandomAgent{hands: hands});
     }
 
-    // Playing NUM_GAMES times game
+    // Having agents play the card game "Hearts" NUM_GAMES times.
     for _ in 1..=NUM_GAMES {
 
-        // Deal cards to each player
-        deal_cards(&mut players);
+        // Dealing cards to each agent.
+        let dealt_cards = deal_cards(&mut players);
+
+        // Getting the playing sequence in the first trick based on agents' hands.
+        // (the agent who has C-2 is the leading player in the initial trick).
+        let mut idx = 0;
+        for (i, val) in dealt_cards.iter().enumerate() {
+            if *val == 0 {
+                idx = i;
+                break;
+            }
+        }
+        let mut winner = idx / NUM_KC;
+        
+        for i in 0..NUM_PLAYERS {
+            println!("{:?}", players[i].hands);
+        }
+        
+        for trick in 0..NUM_KC {
+            
+            for turn in 0..NUM_PLAYERS {
+
+                let card = players[turn].select_card();
+                print!("{}, ", card);
+                
+            }
+            println!("");
+            
+        }
         
         for i in 0..NUM_PLAYERS {
             println!("{:?}", players[i].hands);
@@ -38,7 +65,7 @@ fn main() {
 }
 
 
-fn deal_cards(players: &mut Vec<RandomAgent>) {
+fn deal_cards(players: &mut Vec<RandomAgent>) -> Vec<i32> {
 
     let mut v: Vec<i32> = (0..NUM_CARDS as i32).collect();
     let mut rng = rand::thread_rng();
@@ -47,6 +74,8 @@ fn deal_cards(players: &mut Vec<RandomAgent>) {
     for i in 0..NUM_PLAYERS {
         players[i].set_hands(&v[(i * NUM_KC)..((i+1) * NUM_KC)]);
     }
+
+    return v;
     
 }
 
@@ -60,6 +89,18 @@ impl RandomAgent {
 
     fn set_hands(&mut self, cards: &[i32]) {
         self.hands = cards.try_into().unwrap();
+    }
+
+    fn select_card(&mut self) -> i32 {
+        let mut rng = rand::thread_rng();
+        loop {
+            let card_index = rng.gen_range(0..NUM_KC);
+            if self.hands[card_index] != -1 {
+                let card = self.hands[card_index];
+                self.hands[card_index] = -1;
+                return card;
+            }
+        }
     }
     
 }
